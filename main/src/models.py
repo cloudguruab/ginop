752d60e1d33f26b26e963f0c7dd3a9d69a446b21
cloudguruab <ap.brown011@gmail.com>
2021-12-15 14:14:14 -0500

    configured table schema for api

--- main/src/models.py
+++ main/src/models.py
@@ -1,26 +1,19 @@
-from config import Config
-from sqlalchemy import create_engine, Column
-from sqlalchemy.ext.declarative import declarative_base  
-from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
-db_string = Config.DATBASE
+db = SQLAlchemy()
 
-db = create_engine(db_string)  
-base = declarative_base()
-
-class Film(base):  
+class GinopDB(db.Model):
+    
     __tablename__ = 'ginop'
-
-    id = None
-    created_at = None  # TODO:
-    signed_at = None  
-    signature = None 
-    workflow = None  
+    
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+    signed_at = db.Column(db.DateTime, nullable=False)
+    signature_key = db.Column(db.String(255), nullable=False)
+    workflow_type = db.Column(db.String(255), nullable=False)
     # geospacial = bool  
     # sms = bool 
     # biometric = bool
-
-Session = sessionmaker(db)  
-session = Session()
-
-base.metadata.create_all(db)
+    
+    def __repr__(self):
+        return f'<GinopDB {self.signature_key}'
