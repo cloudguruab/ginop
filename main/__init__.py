9aa414754d442a7059c85a3139b2b097814d820d
cloudguruab <ap.brown011@gmail.com>
2021-12-15 00:04:49 -0500

    corrected blueprints ;

--- main/__init__.py
+++ main/__init__.py
@@ -6,11 +6,14 @@ from config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__, static_folder="./static", template_folder="./templates")
-    app.config.from_mapping(config_class)
+    app.config.from_object(config_class)
     
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+    
+    from main.src import bp as main_bp
+    app.register_blueprint(main_bp)
     
     return app
