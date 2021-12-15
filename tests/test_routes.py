35704b63ec5f6bd0b4c6ee6d6ab112d4b01b1e66
cloudguruab <ap.brown011@gmail.com>
2021-12-15 01:01:56 -0500

    testcases for routes

---
+++ tests/test_routes.py
@@ -0,0 +1,22 @@
+import pytest
+from ..main.src import routes
+
+class TestRoutes:
+    
+    def setup_method(self):
+        pass
+    
+    def teardown_method(self):
+        pass
+    
+    def test_clock_in_valid(self):
+        pass
+    
+    def test_clock_in_rejection(self):
+        pass  
+    
+    def test_clock_out_valid(self):
+        pass
+    
+    def test_clock_out_rejection(self):
+        pass  
