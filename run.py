2ad11896f2467e5e915d23757c96cb1aa14d150c
cloudguruab <ap.brown011@gmail.com>
2021-12-17 11:06:43 -0500

    logging issue

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from main import create_app
+import logging
+import logging.config
 
 app = create_app()
 
 if __name__ == '__main__':
+    # logging.config.fileConfig('logging.conf')
     app.run(port=5000, debug=True)
     
