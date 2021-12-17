14664a364d5f69658bb441788cba994c15359ad7
cloudguruab <ap.brown011@gmail.com>
2021-12-17 13:53:18 -0500

    pulling logging info also update PyYaml import

--- run.py
+++ run.py
@@ -1,10 +1,25 @@
 from main import create_app
 import logging
 import logging.config
+import yaml
+from os.path import join, dirname, realpath
 
+def manager():
+    """
+    manager for application logging
+    """
+    #parse yaml file
+    config_path = join(dirname(realpath(__file__)), 'log/conf.yml')
+    config = yaml.safe_load(open(config_path))
+
+    #configure loggging
+    log_config = config['logging']
+    logging.config.dictConfig(log_config) 
+    logger = logging.getLogger(__name__)
+    
 app = create_app()
 
 if __name__ == '__main__':
-    # logging.config.fileConfig('logging.conf')
+    manager()
     app.run(port=5000, debug=True)
     
