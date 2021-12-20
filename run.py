e4244e5972111368e07c19772750a5f59f6565f6
cloudguruab <ap.brown011@gmail.com>
2021-12-19 20:59:56 -0500

    chaing logic for factory

--- run.py
+++ run.py
@@ -2,15 +2,16 @@ from main import create_app
 import logging
 import logging.config
 import yaml
-from os.path import join, dirname, realpath
+import os 
 
 def manager():
     """
     manager for application logging
     """
     #parse yaml file
-    config_path = join(dirname(realpath(__file__)), 'log/conf.yml')
-    config = yaml.safe_load(open(config_path))
+    path = os.path.dirname(os.path.realpath(__file__))
+    loggingConf = f"{path}/log/conf.yml"
+    config = yaml.safe_load(loggingConf)
 
     #configure loggging
     log_config = config['logging']
