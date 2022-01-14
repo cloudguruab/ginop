3afbc7b20a13888d975b5753a5c152fc628e0b7a
cloudguruab <ap.brown011@gmail.com>
2022-01-13 22:43:14 -0500

    moved db.conf

--- main/__init__.py
+++ main/__init__.py
@@ -3,6 +3,9 @@
 import os, logging
 from flask import Flask, current_app
 from config import Config 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__, static_folder="./static", template_folder="./templates")
@@ -16,6 +19,11 @@ def create_app(config_class=Config):
     except OSError:
         logger.info("instance folder %s already exist" % app.instance_path)
         pass
+    
+    with app.app_context():
+        current_app.config['SQLALCHEMY_DATABASE_URI'] = config_class.DATABASE
+        db.init_app(app)
+        db.create_all()
 
     from main.main_app import bp as main_bp
     app.register_blueprint(main_bp)
