af7c4648416a5f1d86d715dd127b037bcba4a503
cloudguruab <ap.brown011@gmail.com>
2022-01-15 13:07:39 -0500

    updated project structure to django 4.0

--- ginope/api/dev/blockchain/common/exceptions.py
+++
@@ -1,6 +0,0 @@
-"""global exceptions for ginop blockchain"""
-
-class ChainConnectionException(Exception):
-    """
-    handler for failed connections to blockchain platform.
-    """
--- main/main_app/__init__.py
+++
@@ -1,6 +0,0 @@
-''' Blueprint for main application functionality. '''
-from flask import Blueprint
-
-bp = Blueprint('src', __name__, template_folder='../templates')
-
-from main.main_app import routes
