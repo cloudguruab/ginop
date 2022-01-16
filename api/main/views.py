b88aad93e4e1927662df866a7505512560a7374c
cloudguruab <ap.brown011@gmail.com>
2022-01-15 19:27:53 -0500

    conf user viewset but will turn into clock in and out endpoint/mixin

--- api/main/views.py
+++ api/main/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+import logging, time, json, os, datetime
+from ..dev.blockchain import chain
+from ..dev.common.exceptions import FailedChainEventExeception
+from .schema import UserSerializer
 
-# Create your views here.
-# import logging, time, json, os, datetime
-# from main.main_app import bp
-# from flask import render_template, redirect
-# import os.path, sys
+# logger = logging.getLogger(__name__) NOTE: config logger
+blockchain = chain.Blockchain()
 
-# sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-# from blockchain.private import chain
+class UserView(viewsets.ModelViewSet):
+    #NOTE: make this your clock in and out endpoints 
+    """
+    Endpoint that allows users to be viewed or edisted
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
 
-# logger = logging.getLogger(__name__)
-# blockchain = chain.Blockchain()
 
-# #initial route
-# @bp.route('/', methods=['GET', 'POST'])
-# def index():
-#     return render_template('index.html')    
+    
 
 # @bp.route('/chain', methods=['GET'])
 # def private_chain():
@@ -24,22 +29,3 @@ from django.shortcuts import render
 #         chain_data.append(block.__dict__)
 #     return json.dumps({"length": len(chain_data),
 #                        "chain": chain_data})
-    
-# @bp.route('/register', methods=['GET', 'POST'])
-# def register(): #TODO:
-#     return render_template('index.html')
-    
-# @bp.route('/clock-in', methods=[''])
-# def clock_in():
-    # time clocked in 
-    # nonce generated
-    # transaction is initialized but not completed  
-#     pass
-
-# @bp.route('/clock-out')
-# def clock_out():
-    # time clocked out 
-    # verify nonce exist for specific transaction
-    # read what transactions exists
-    # transaction is completed 
-#     pass
