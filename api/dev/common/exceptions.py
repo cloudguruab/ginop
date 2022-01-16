378a6cd0ca1dd441be1ed17836648922ba50a4c1
cloudguruab <ap.brown011@gmail.com>
2022-01-15 19:26:27 -0500

    added new exception handler for view and viewsets

--- api/dev/common/exceptions.py
+++ api/dev/common/exceptions.py
@@ -4,3 +4,8 @@ class ChainConnectionException(Exception):
     """
     handler for failed connections to blockchain platform.
     """
+
+class FailedChainEventExeception(Exception):
+    """
+    handler for failed blockchain actions on the ginop api.
+    """
